b4764410a59e538e2c6fe623b8969bb0f71d82c5
Alina Zolotavina <abekmetova@yandex.ru>
2022-02-19 23:35:12 +0400

    feat: open and close adding card popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
-const profileEditBtn = document.querySelector('.profile__edit-btn');
-const popup = document.querySelector('.popup');
-const popupCloseBtn = popup.querySelector('.popup__close-btn');
-
-// open popup
-const openPopup = function() {
-    popup.classList.add('popup_is-opened');
+// open popup function
+const openPopup = function(popupElement) {
+  popupElement.classList.add('popup_is-opened');
 };
 
-profileEditBtn.addEventListener('click', openPopup);
-
-// close popup
-const closePopup = function() {
-    popup.classList.remove('popup_is-opened');
+// close popup function
+const closePopup = function(popupElement) {
+  popupElement.classList.remove('popup_is-opened');
 };
 
-popupCloseBtn.addEventListener('click', closePopup);
+// define profile editing popup's elements
+const editProfilePopup = document.querySelector('.popup_type_edit-profile');
+const editProfileBtn = document.querySelector('.profile__edit-btn');
+const editProfilePopupCloseBtn = editProfilePopup.querySelector('.popup__close-btn');
 
-// activate like-btn
-for (let likeBtn of  document.querySelectorAll('.element__like-btn')) {
-    likeBtn.addEventListener('click', function() {
-        likeBtn.classList.add('element__like-btn_clicked');
-    });
-};
+// open profile editing popup
+editProfileBtn.addEventListener('click', function() {
+  openPopup(editProfilePopup);
+});
+
+// close profile editing popup
+editProfilePopupCloseBtn.addEventListener('click', function() {
+  closePopup(editProfilePopup);
+});
 
 // edit profile information
-const formElement = popup.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_job');
+const editFormElement = editProfilePopup.querySelector('.popup__form');
+const nameInput = editFormElement.querySelector('.popup__input_type_name');
+const jobInput = editFormElement.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
@@ -36,7 +36,29 @@ function handleProfileFormSubmit (event) {
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-    closePopup();
+    closePopup(editProfilePopup);
 };
 
-formElement.addEventListener('submit', handleProfileFormSubmit);
+editFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+// define card adding popup's elements
+const addCardPopup = document.querySelector('.popup_type_add-photo');
+const addCardBtn = document.querySelector('.profile__add-btn');
+const addCardPopupCloseBtn = addCardPopup.querySelector('.popup__close-btn');
+
+// open card adding popup
+addCardBtn.addEventListener('click', function() {
+  openPopup(addCardPopup);
+});
+
+// close card adding popup
+addCardPopupCloseBtn.addEventListener('click', function() {
+  closePopup(addCardPopup);
+});
+
+// activate like-btn
+for (let likeBtn of  document.querySelectorAll('.element__like-btn')) {
+  likeBtn.addEventListener('click', function() {
+      likeBtn.classList.add('element__like-btn_clicked');
+  });
+};
